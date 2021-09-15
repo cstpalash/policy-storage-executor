8e1550801f4f1932d9248141c0197d2e5ccf1f2f
cstpalash <cstpalash@gmail.com>
2021-09-15 19:46:01 +0530

    added dynamodb backend

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-const { getItem, putItem } = require("./policyExecutionService.js");
+const { GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBClient } =  require("@aws-sdk/client-dynamodb");
+// Set the AWS Region.
+const REGION = "us-west-2"; //e.g. "us-east-1"
+// Create an Amazon DynamoDB service client object.
+const ddbClient = new DynamoDBClient({ region: REGION });
+
+const { DynamoDBDocumentClient } =  require("@aws-sdk/lib-dynamodb");
+
+const marshallOptions = {
+    // Whether to automatically convert empty strings, blobs, and sets to `null`.
+    convertEmptyValues: true, // false, by default.
+    // Whether to remove undefined values while marshalling.
+    removeUndefinedValues: true, // false, by default.
+    // Whether to convert typeof object to map attribute.
+    convertClassInstanceToMap: false, // false, by default.
+};
+
+const unmarshallOptions = {
+    // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
+    wrapNumbers: false, // false, by default.
+};
+
+const translateConfig = { marshallOptions, unmarshallOptions };
+
+// Create the DynamoDB Document client.
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, translateConfig);
+
 
 exports.handler = async function(event, context) {
   
@@ -105,6 +132,44 @@ function getRecord(body){
   };
 
   return record; 
+}
+
+const getItem = async function (eventID, time){
+  const params = {
+    TableName: "tbl_policy_execution",
+    Key: {
+      eventID: eventID,
+      time: time
+    }
+  };
+
+  let data = null;
+
+  try {
+    data = await ddbDocClient.send(new GetCommand(params));
+  } catch (err) {
+    console.log("Error", err);
+  }
+
+  return data.Item;
+}
+
+const putItem = async function (record){
+
+  let params = { 
+    TableName: "tbl_policy_execution",
+    Item : record
+  };
+
+  let data = null;
+
+  try{
+    data = await ddbDocClient.send(new PutCommand(params));
+  } catch (err){
+    console.log("Error", err);
+  }
+
+  return data;
 }
 
 
