794ffe2b9043e811b04b4d52b43dd1e24feec236
cstpalash <cstpalash@gmail.com>
2021-09-16 10:23:07 +0530

    validate policy

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const { GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDBClient } =  require("@aws-sdk/client-dynamodb");
+const { S3Client, GetBucketEncryptionCommand } = require("@aws-sdk/client-s3");
+const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
+
 // Set the AWS Region.
 const REGION = "us-west-2"; //e.g. "us-east-1"
 // Create an Amazon DynamoDB service client object.
 const ddbClient = new DynamoDBClient({ region: REGION });
-
 const { DynamoDBDocumentClient } =  require("@aws-sdk/lib-dynamodb");
-
 const marshallOptions = {
     // Whether to automatically convert empty strings, blobs, and sets to `null`.
     convertEmptyValues: true, // false, by default.
@@ -15,17 +16,17 @@ const marshallOptions = {
     // Whether to convert typeof object to map attribute.
     convertClassInstanceToMap: false, // false, by default.
 };
-
 const unmarshallOptions = {
     // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
     wrapNumbers: false, // false, by default.
 };
-
 const translateConfig = { marshallOptions, unmarshallOptions };
-
 // Create the DynamoDB Document client.
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient, translateConfig);
 
+const s3Client = new S3Client({ REGION : REGION });
+
+const lambdaClient = new LambdaClient({ REGION : REGION });
 
 exports.handler = async function(event, context) {
   
@@ -53,20 +54,16 @@ async function process_aws_s3_event(body){
 
   switch(body.detail.eventName){
     case "PutBucketEncryption":
-      await process_aws_s3_event_PutBucketEncryption(body);
-      break; 
     case "DeleteBucketEncryption":
-      await process_aws_s3_event_DeleteBucketEncryption(body);
-      break;
     case "CreateBucket":
-      await process_aws_s3_event_CreateBucket(body);
-      break;
+      await process_aws_s3_event(body);
+      break; 
     default:
       break;
   }
 }
 
-async function process_aws_s3_event_PutBucketEncryption(body){
+async function process_aws_s3_event(body){
 
   let eventID = body.detail.eventID;
   let time = body.detail.eventTime;
@@ -78,43 +75,17 @@ async function process_aws_s3_event_PutBucketEncryption(body){
 
     let record = getRecord(body); 
 
-    let addedItem = await putItem(record);
+    let encryptionConfig = await getEncryptionConfiguration(record.resource);
 
-  }
+    if(encryptionConfig){
 
-}
+      let policyResponse = await validatePolicy(encryptionConfig);
 
-async function process_aws_s3_event_DeleteBucketEncryption(body){
+      let validationResult = binArrayToJson(policyResponse.Payload);
 
-  let eventID = body.detail.eventID;
-  let time = body.detail.eventTime;
+      await putItem(Object.assign(record, validationResult));
 
-  let itemPresent = await getItem(eventID, time);
-
-  if(!itemPresent){
-    //we have NOT processed this event earlier
-
-    let record = getRecord(body); 
-
-    let addedItem = await putItem(record);
-
-  }
-
-}
-
-async function process_aws_s3_event_CreateBucket(body){
-
-  let eventID = body.detail.eventID;
-  let time = body.detail.eventTime;
-
-  let itemPresent = await getItem(eventID, time);
-
-  if(!itemPresent){
-    //we have NOT processed this event earlier
-
-    let record = getRecord(body); 
-
-    let addedItem = await putItem(record);
+    }
 
   }
 
@@ -172,9 +143,57 @@ const putItem = async function (record){
   return data;
 }
 
+const getEncryptionConfiguration = async function(bucketName){
+  let response = null;
+
+  try{
+
+    response = await s3Client.send(new GetBucketEncryptionCommand({ Bucket : bucketName }));
+
+  } catch (err){
+      if(err.Code == "ServerSideEncryptionConfigurationNotFoundError"){
+        response = {};
+      }
+      else {
+        console.log(err.Code);
+      }
+  }
+
+  return response;
+}
+
+const validatePolicy = async function(config){
+
+  let response = null;
+  let payload = serialize({
+    "action" : "validate-aws-s3-encryption-configuration",
+    "metadata" : config
+  });
+
+  try{
+    response = await lambdaClient.send(new InvokeCommand({
+      FunctionName : "fn-policy-storage-encryption",
+      Payload : payload
+    }));
+  } catch(err){
+    console.log("Error", err);
+  }
+
+  return response;
+}
+
 
 // AWS - S3 end
 
 var serialize = function(object) {
   return JSON.stringify(object, null, 2)
+}
+
+var binArrayToJson = function(binArray)
+{
+  var str = "";
+  for (var i = 0; i < binArray.length; i++) {
+    str += String.fromCharCode(parseInt(binArray[i]));
+  }
+  return JSON.parse(str)
 }
